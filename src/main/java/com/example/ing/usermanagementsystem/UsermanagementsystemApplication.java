f894bab8b87bc9aa8b478fe4b33f35b021c38d2f
Aastha <aasthabajaj1703@gmail.com>
2024-09-23 16:51:16 +0530

    Initial commit

---
+++ src/main/java/com/example/ing/usermanagementsystem/UsermanagementsystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.ing.usermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementsystemApplication.class, args);
+		System.out.println("My First Task");
+	}
+
+}
